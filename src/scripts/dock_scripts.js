2999ce37e4ec23c9a43abc626ccc878c0dc41649
Kensley Lewis <klpsupport@gmail.com>
2019-10-10 09:02:43 -0400

    - cleaned up js file - cleaned up scss active class as well as add to cart styling

--- src/scripts/dock_scripts.js
+++ src/scripts/dock_scripts.js
@@ -1,7 +1,12 @@
 "use strict";
 import $ from "jquery";
 
-console.log('js connected via webpack');
+//////
+//JQ- Test
+function testjs() {
+    console.log('js connected via webpack');
+    $('html').addClass('jq-enabled');
+}
 
 /////////////////////////////////////////
 //DETECT IF CSS3 ANIMATIONS ARE SUPPORTED
@@ -20,50 +25,54 @@ function animatetopbar() {
         }
     }
 }
-animatetopbar();
+
+//////////////////////////////////////////////////////
+//JQ- DEV MODE - DISABLE ALL ANCHORLINKS DURING DEVELOPMENT
+function disableLinks() {
+    $('a').on('click', function (e) {
+        e.preventDefault();
+    });
+}
 
 
-///////////
-(function () {
-    //CONFIRM JQUERY WORKS
-    $('html').addClass('jq-enabled');
-
-     //INITIATE BS4 TOOLTIPS | https://getbootstrap.com/docs/4.0/components/tooltips/
+////////////////////
+//JQ - RUN BS4 JS ACTIONS
+function bs4actions() {
+    //INITIATE BS4 TOOLTIPS | https://getbootstrap.com/docs/4.0/components/tooltips/
     //use css to disable them on mobile devices | https://stackoverflow.com/a/26689836/957186
-    //$('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
+}
 
-     //DISABLE ALL ANCHORLINKS DURING DEVELOPMENT
-     function disableLinks() {
-        $('a').on('click', function (e) {
-            e.preventDefault();
-        });
-    }
+////////////////
+//JQ - SCROLL TOP BTN
+function scrollbackup() {
+    
+    //GET AND FADE ELEMENT
+    var scrollTop = $(".scroll-top-btn");
+    $(window).scroll(function () {
+        var topPos = $(this).scrollTop();
+        if (topPos > 20) {
+            $(scrollTop).fadeIn();
+        } else {
+            $(scrollTop).fadeOut();
+        }
+    });
+
+    //SCROLL PAGE
+    $(scrollTop).click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+}
+
+/////////////////
+//RUN ALL SCRIPTS
+(function () {
+    testjs()
+    animatetopbar();
     disableLinks();
-
-
-    
-    function scrollbackup() {
-        var scrollTop = $(".scroll-top-btn");
-        $(window).scroll(function () {
-    
-            var topPos = $(this).scrollTop();
-            if (topPos > 20) {
-                $(scrollTop).fadeIn();
-            } else {
-                $(scrollTop).fadeOut();
-            }
-        }); // scroll END
-    
-        //Click event to scroll to top
-        $(scrollTop).click(function () {
-            $('html, body').animate({
-                scrollTop: 0
-            }, 400);
-            return false;
-        }); // click() scroll top EMD
-    }
-    
     scrollbackup();
-    
-
+    bs4actions();
 })($);
